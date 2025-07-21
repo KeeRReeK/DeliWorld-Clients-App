d50d830b15193aadbf31dfb9e8cbe0dcbfdd246b
KeeRReeK <valentinreek@gmail.com>
2025-07-21 19:56:21 +0300

    chore: initial project setup

---
+++ FoodDelivery/FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by KeeR ReeK on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
