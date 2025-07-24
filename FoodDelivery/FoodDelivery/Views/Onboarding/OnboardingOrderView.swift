6ef38d53674549b9b2c4c8c797b3752c2b8ff612
KeeRReeK <valentinreek@gmail.com>
2025-07-24 15:53:31 +0300

    feat: add OnboardingOrderView

---
+++ FoodDelivery/FoodDelivery/Views/Onboarding/OnboardingOrderView.swift
@@ -0,0 +1,101 @@
+//
+//  OnboardingOrderView.swift
+//  FoodDelivery
+//
+//  Created by KeeR ReeK on 22.07.2025.
+//
+
+import SwiftUI
+
+struct OnboardingOrderView: View {
+    @State var isPresented: Bool = true
+    
+    var body: some View {
+        ZStack {
+            Image("Background Onboarding Order")
+                .resizable()
+                .ignoresSafeArea()
+                
+            
+            VStack {
+                
+                Spacer()
+            }
+            
+            VStack {
+                Spacer()
+                RoundedRectangle(cornerRadius: 0, style: .continuous)
+                    .fill(Color.white)
+                    .frame(maxHeight: 338)
+                    .overlay(
+                        ScrollView(showsIndicators: false) {
+                            OnboardingOrderContent()
+                        }
+                    )
+                    
+            }
+            .ignoresSafeArea()
+        }
+        .safeAreaInset(edge: .top) {
+            HStack {
+                Spacer()
+                Button("Skip >") {
+                    
+                }
+                .font(.custom("LeagueSpartan-Bold", size: 20))
+                .foregroundColor(.orangeBase)
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+            }
+        }
+    }
+}
+
+struct OnboardingOrderContent: View {
+    var body: some View {
+        VStack(spacing: 30) {
+            
+            Image("Onboarding Order Icon")
+                .font(.system(size: 36))
+                .foregroundColor(.orangeBase)
+                .padding(.top, 23)
+            
+            Text("Order For Food")
+                .font(.custom("Inter-Regular_Black", size: 24))
+                .foregroundColor(.orangeBase)
+            
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.")
+                .font(.custom("LeagueSpartan-Medium", size: 14))
+                .multilineTextAlignment(.center)
+                .foregroundColor(.gray)
+                .padding(.horizontal, 60)
+            
+            HStack(spacing: 8) {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.orangeBase)
+                    .frame(width: 20, height: 4)
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.yellow2)
+                    .frame(width: 20, height: 4)
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.yellow2)
+                    .frame(width: 20, height: 4)
+            }
+            
+            Button("Next") {
+                
+            }
+            .frame(maxWidth: 130)
+            .frame(height: 36)
+            .background(.orangeBase)
+            .foregroundColor(.white)
+            .font(.custom("LeagueSpartan-Medium", size: 17))
+            .clipShape(Capsule())
+            
+        }
+    }
+}
+
+#Preview {
+    OnboardingOrderView()
+}
