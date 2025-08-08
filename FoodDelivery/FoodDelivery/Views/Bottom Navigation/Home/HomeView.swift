3ffe8255cc820a6824eff9b5ff5c2c2a7dc124a5
KeeRReeK <valentinreek@gmail.com>
2025-08-08 15:46:23 +0300

    feat: add resources for MenuView

--- FoodDelivery/FoodDelivery/Views/Bottom Navigation/Home/HomeView.swift
+++ FoodDelivery/FoodDelivery/Views/Bottom Navigation/Home/HomeView.swift
@@ -24,24 +24,6 @@ struct HomeView: View {
                         .padding()
                         .background(Color.white)
                         .cornerRadius(30)
-                        .overlay {
-                            HStack {
-                                Spacer()
-                                Button {
-                                    
-                                } label: {
-                                    ZStack {
-                                        Circle()
-                                            .padding(6)
-                                            .foregroundColor(.orangeBase)
-                                        Image("Filter Icon")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(maxHeight: 12)
-                                    }
-                                }
-                            }
-                        }
                         .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
                     
                     Button {
@@ -141,58 +123,7 @@ struct HomeView: View {
     }
 }
 
-struct HomeViewContent: View {
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            
-            HStack {
-                Text("Best Seller")
-                    .font(.system(size: 25, weight: .medium))
-                Spacer()
-                Button {
-                    
-                } label: {
-                    Text("View All  >")
-                        .font(.custom("LeagueSpartan-Medium", size: 17))
-                        .foregroundStyle(.orangeBase)
-                }
-            }
-            
-            ScrollView(.horizontal) {
-                HStack(spacing: 15) {
-                    BestSellerItemView(image: "Food Card Best Seller", price: "103,00")
-                        .padding(.bottom, 15)
-                    BestSellerItemView(image: "Food Card Best Seller@2", price: "50,00")
-                        .padding(.bottom, 15)
-                    BestSellerItemView(image: "Food Card Best Seller", price: "12,99")
-                        .padding(.bottom, 15)
-                    BestSellerItemView(image: "Food Card Best Seller@2", price: "8,20")
-                        .padding(.bottom, 15)
-                    BestSellerItemView(image: "Food Card Best Seller", price: "103,00")
-                        .padding(.bottom, 15)
-                }
-            }
-            
-            SliderDiscountView()
-                .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
-            
-            Text("Recommend")
-                .font(.system(size: 25, weight: .medium))
-            
-            ScrollView(.horizontal) {
-                HStack(spacing: 20) {
-                    RecommendItemView(raiting: 5, image: "Food Recommend Card", price: "25.00")
-                    RecommendItemView(raiting: 5, image: "Food Recommend Card", price: "25.00")
-                }
-                .padding(.bottom, 15)
-            }
-            
-        }
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding()
-    }
-}
+
 
 #Preview {
     HomeView()
