e2242e66c82f5b3b7e200ded76f293dea3e4fd51
KeeRReeK <valentinreek@gmail.com>
2025-08-19 15:33:27 +0300

    feat: add UI for App Settings views

---
+++ DeliWorld-Clients-App/Views/App Settings/SettingsView.swift
@@ -0,0 +1,53 @@
+//
+//  SettingsView.swift
+//  DeliWorldClients
+//
+//  Created by KeeR ReeK on 19.08.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    var body: some View {
+        ZStack {
+            Color(.yellowBase)
+                .ignoresSafeArea()
+                .padding(.bottom, 20)
+            VStack {
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Text("<")
+                            .font(.system(size: 30, weight: .bold, design: .default))
+                            .foregroundStyle(.orangeBase)
+                    }
+                    Spacer()
+                    Text("Settings")
+                        .font(.system(size: 30, weight: .bold, design: .default))
+                        .foregroundStyle(.white)
+                    Spacer()
+                }
+                .padding()
+                
+                RoundedRectangle(cornerRadius: 24, style: .continuous)
+                    .fill(Color.white)
+                    .ignoresSafeArea()
+                    .overlay(
+                        ScrollView {
+                            SettingsViewContent()
+                                .padding(.top, 10)
+                        }
+                    )
+            }
+        }
+    }
+}
+#Preview {
+    RecommendationsView()
+}
+
+#Preview {
+    SettingsView()
+}
